2815f878ec59df210667f64cf1979621b8ed92d3
Ashley Sese <sese.ashrean@gmail.com>
2023-01-09 16:13:29 -0800

    created current day and if/else statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,6 +7,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +19,29 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  //displays the current day //
+  var now = dayjs().format('MM/DD/YYYY')
+  $('#currentDay').text(now)
+
+  // created a variable selecting the id and splitting it so we can isolate the number // 
+  var hour = $('#hour-9').attr('id').split('-')[1]
+  var currentHour = dayjs().hour()
+
+  var txtHour = $('#hour-9')
+
+  if (hour < currentHour) {
+    txtHour.addClass('past')
+    txtHour.removeClass('present future')
+
+  } else if (hour === currentHour) {
+    txtHour.removeClass('past future')
+    txtHour.addClass('present')
+
+  } else {
+    txtHour.addClass('future')
+    txtHour.removeClass('past present')
+  }
+
+
 });
